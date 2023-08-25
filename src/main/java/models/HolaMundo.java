c57b2abeddff5b9d20b051f10c66a68d3e40e54a
agusvidal <agustinacandela2300@gmail.com>
2023-08-25 18:05:39 -0300

    cambios de prueba

---
+++ src/main/java/models/HolaMundo.java
@@ -0,0 +1,19 @@
+package models;
+
+public class HolaMundo {
+
+	public String holaMundo;
+
+    public HolaMundo() {
+    	holaMundo = "¡Hola Mundo desde el Modelo!";
+    }
+    
+	public String getHolaMundo() {
+		return holaMundo;
+	}
+
+	public void setHolaMundo(String holaMundo) {
+		this.holaMundo = holaMundo;
+	}
+	
+}
