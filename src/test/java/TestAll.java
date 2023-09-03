b09c267a0716bd84bc9ee73b2f7c93e195d2c68a
agusvidal <agustinacandela2300@gmail.com>
2023-09-03 16:04:25 -0300

    cambios en test

---
+++ src/test/java/TestAll.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class TestAll {
+        @Test
+        public void testQueNoHaceNada() {
+            // Este test no hace nada, pero está anotado con @Test
+        }
+}
