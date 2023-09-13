5367d22fe881cda952d7b36ae38ef8971271d6ca
agusvidal <agustinacandela2300@gmail.com>
2023-09-13 18:58:58 -0300

    cambio de init core

--- src/main/java/InitCore.java
+++ src/main/java/InitCore.java
@@ -4,13 +4,14 @@ import model.ObservableChefExpress;
 public class InitCore {
     protected String propertyPath;
     protected String defaultConfig;
+    protected ChefExpressFactory factory;
         public InitCore(String propertyPath, String defaultConfig) throws Exception {
             this.propertyPath = propertyPath;
             this.defaultConfig = defaultConfig;
+             factory = new ChefExpressFactory(propertyPath, defaultConfig);
 
         }
         public ObservableChefExpress init() throws Exception {
-            ChefExpressFactory factory = new ChefExpressFactory(propertyPath, defaultConfig);
             return factory.createChefExpress();
         }
 }
