ad90260c81c79a5edacb4be6e5de6775b28bc65e
agusvidal <agustinacandela2300@gmail.com>
2023-09-13 10:23:36 -0300

    Se agrega el parser

---
+++ src/main/java/MainPruebaParser.java
@@ -0,0 +1,20 @@
+import entities.Recipe;
+import parsers.RecipeParser;
+
+import java.util.List;
+
+public class MainPruebaParser {
+
+    public static void main(String[] args) {
+        RecipeParser p = new RecipeParser();
+        List<Recipe> recetas = p.parserRecipes();
+
+        // Puedes trabajar con la lista de objetos Recipe aquí
+        for (Recipe recipe : recetas) {
+            System.out.println("ID: " + recipe.getId());
+            System.out.println("Nombre: " + recipe.getName());
+            System.out.println("Instrucciones: " + recipe.getIngredients());
+            System.out.println();
+        }
+    }
+}
