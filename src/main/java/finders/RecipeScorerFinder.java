dafe84dd914005b74e1e946dd18fc6926c9b3e06
cristianFleita <cristianagustinfleita@gmail.com>
2023-09-14 16:08:45 -0300

    Added new test cases to US2

--- src/main/java/finders/RecipeScorerFinder.java
+++ src/main/java/finders/RecipeScorerFinder.java
@@ -3,6 +3,7 @@ package finders;
 import interfaces.RecipeScorer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
@@ -16,7 +17,7 @@ import java.util.stream.Collectors;
 public class RecipeScorerFinder
 {
     public String directory;
-    private static final String extentionJars = ".jar";
+    private static final String validFileExtension = ".jar";
 
 
     public RecipeScorerFinder(String url)
@@ -24,55 +25,49 @@ public class RecipeScorerFinder
         this.directory = url;
     }
 
-    public Set<RecipeScorer> find()
+    public  Map<String, RecipeScorer>  find() throws FileNotFoundException
     {
-        HashSet<RecipeScorer> scorers = new HashSet<>();
-
+        Map<String, RecipeScorer> scorers = new HashMap<>();
         List<File> filejars = findFileJars(this.directory);
-        if (filejars.isEmpty())
-            return null;
 
-        try{
+        try
+        {
             URL[] urlsList = getUrls(filejars);
             URLClassLoader ClassesLoader = new URLClassLoader(urlsList);
             Set<Class<?>> classes = findClassesInJar(filejars);
+
             for(Class<?> element : classes)
             {
                 if (RecipeScorer.class.isAssignableFrom(element))
-                {
-                    scorers.add((RecipeScorer) element.getDeclaredConstructor().newInstance() );
-                }
+                    scorers.put(element.getName(), (RecipeScorer) element.getDeclaredConstructor().newInstance() );
             }
-
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
+        }
+        catch (MalformedURLException | InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException e)
+        {
             throw new RuntimeException(e);
         }
 
         return scorers;
-
     }
 
     private Set<Class<?>> findClassesInJar(List<File> filejars)
     {
         Set<Class<?>> classes = new HashSet<>();
 
-        for(File file : filejars) {
-            try (JarFile jarFile = new JarFile(file)) {
+        for(File file : filejars)
+        {
+            try (JarFile jarFile = new JarFile(file))
+            {
                 URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{file.toURI().toURL()});
 
                 Enumeration<JarEntry> entries = jarFile.entries();
 
-                while (entries.hasMoreElements()) {
+                while (entries.hasMoreElements())
+                {
                     JarEntry entry = entries.nextElement();
-                    if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
+                    if (!entry.isDirectory() && entry.getName().endsWith(".class"))
+                    {
                         String className = entry.getName().replace("/", ".").replace(".class", "");
 
                         Class<?> cls = Class.forName(className, true, classLoader);
@@ -80,27 +75,34 @@ public class RecipeScorerFinder
                     }
                 }
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (ClassNotFoundException e) {
+            } catch (IOException | ClassNotFoundException e)
+            {
                 throw new RuntimeException(e);
             }
-
         }
 
         return  classes;
     }
 
-    private List<File> findFileJars(String pathFile) {
+    private List<File> findFileJars(String pathFile) throws FileNotFoundException
+    {
         File fileJar = new File(pathFile);
+        if (!fileJar.exists())
+            throw  new FileNotFoundException();
+
+        if(!fileJar.isDirectory())
+            throw  new IllegalArgumentException();
+
         File[] fileList = Optional.of(fileJar).map(File::listFiles)
                 .orElse(new File[] {});
+
         return Arrays.asList(fileList).stream()
-                .filter(i -> i.getPath().endsWith(extentionJars))
+                .filter(i -> i.getPath().endsWith(validFileExtension))
                 .collect(Collectors.toList());
     }
 
-    private URL[] getUrls(List<File> filejars) throws MalformedURLException {
+    private URL[] getUrls(List<File> filejars) throws MalformedURLException
+    {
         URL[] urlsList = new URL[filejars.size()];
         for (int i = 0; i < filejars.size(); i++) {
             urlsList[i] = filejars.get(i).toURI().toURL();
--- src/test/java/IT0/US2.java
+++ src/test/java/IT0/US2.java
@@ -4,16 +4,59 @@ import finders.RecipeScorerFinder;
 import interfaces.RecipeScorer;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.util.Map;
 import java.util.Set;
 
 public class US2
 {
-        @Test
-        public void CA5_Criterio_Simple()
-        {
-                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/oneImpl");
-                Set<RecipeScorer> scorers = finder.find();
-                assert (scorers.size() == 1);
+        @Test(expected = FileNotFoundException.class)
+        public void CA1() throws Exception {
+                RecipeScorerFinder finder = new RecipeScorerFinder("NoExiste");
+                Map<String, RecipeScorer> scorers = finder.find();
         }
 
+        @Test(expected = IllegalArgumentException.class)
+        public void CA2() throws Exception
+        {
+                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/archivo.txt");
+                Map<String, RecipeScorer> scorers = finder.find();
+        }
+
+        @Test
+        public void CA3() throws Exception
+        {
+                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/carpetaVacia");
+                Map<String, RecipeScorer> scorers = finder.find();
+                assert (scorers.isEmpty());
+        }
+
+        @Test
+        public void CA4() throws Exception
+        {
+                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/noEsCriterio");
+                Map<String, RecipeScorer> scorers = finder.find();
+                assert (scorers.isEmpty());
+        }
+
+        @Test
+        public void CA5() throws Exception
+        {
+                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/oneImpl");
+                Map<String, RecipeScorer> scorers = finder.find();
+
+                assert (scorers.size() == 1);
+                assert (scorers.containsKey("RecetasSaludables"));
+        }
+
+        @Test
+        public void CA6() throws Exception
+        {
+                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/twoImpl");
+                Map<String, RecipeScorer> scorers = finder.find();
+
+                assert (scorers.size() == 2);
+                assert (scorers.containsKey("RecetasSaludables"));
+                assert (scorers.containsKey("Diabeticos"));
+        }
 }
