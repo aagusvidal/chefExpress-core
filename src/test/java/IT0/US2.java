2347364bf97dee3cc94e516c012816ecba477a7b
cristianFleita <cristianagustinfleita@gmail.com>
2023-09-14 16:41:21 -0300

    Refactored US2 test cases

--- src/test/java/IT0/US2.java
+++ src/test/java/IT0/US2.java
@@ -6,53 +6,61 @@ import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.util.Map;
-import java.util.Set;
 
 public class US2
 {
+        private static String basePath = "build/resources/test/";
+
+        /** @name = Ubicacion inexistente
+         * @exception = FileNotFoundException **/
         @Test(expected = FileNotFoundException.class)
-        public void CA1() throws Exception {
-                RecipeScorerFinder finder = new RecipeScorerFinder("NoExiste");
-                Map<String, RecipeScorer> scorers = finder.find();
+        public void CA1() throws Exception
+        {
+                new RecipeScorerFinder("NoExiste").find();
         }
 
+        /** @name = Ubicacion invalida
+         * @exception = IllegalArgumentException **/
         @Test(expected = IllegalArgumentException.class)
         public void CA2() throws Exception
         {
-                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/archivo.txt");
-                Map<String, RecipeScorer> scorers = finder.find();
+                new RecipeScorerFinder(basePath + "archivo.txt").find();
         }
 
+        /** @name = Carpeta vacia **/
         @Test
         public void CA3() throws Exception
         {
-                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/carpetaVacia");
+                RecipeScorerFinder finder = new RecipeScorerFinder(basePath + "carpetaVacia");
                 Map<String, RecipeScorer> scorers = finder.find();
                 assert (scorers.isEmpty());
         }
 
+
+        /** @name = No es criterio de puntuación **/
         @Test
         public void CA4() throws Exception
         {
-                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/noEsCriterio");
-                Map<String, RecipeScorer> scorers = finder.find();
-                assert (scorers.isEmpty());
+                RecipeScorerFinder finder = new RecipeScorerFinder(basePath + "noEsCriterio");
+                assert (finder.find().isEmpty());
         }
 
+        /** @name = Puntuador simple **/
         @Test
         public void CA5() throws Exception
         {
-                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/oneImpl");
+                RecipeScorerFinder finder = new RecipeScorerFinder(basePath + "oneImpl");
                 Map<String, RecipeScorer> scorers = finder.find();
 
                 assert (scorers.size() == 1);
                 assert (scorers.containsKey("RecetasSaludables"));
         }
 
+        /** @name = Puntuador multiples **/
         @Test
         public void CA6() throws Exception
         {
-                RecipeScorerFinder finder = new RecipeScorerFinder("build/resources/test/twoImpl");
+                RecipeScorerFinder finder = new RecipeScorerFinder(basePath + "twoImpl");
                 Map<String, RecipeScorer> scorers = finder.find();
 
                 assert (scorers.size() == 2);
