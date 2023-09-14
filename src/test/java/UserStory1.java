e2954f183230d5d4fdc3e9b9f218f5734c1191ce
agusvidal <agustinacandela2300@gmail.com>
2023-09-14 10:42:48 -0300

    cambio de nombre

--- src/test/java/UserStory1.java
+++ src/test/java/UserStory1.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class UserStorey1 {
+public class UserStory1 {
     private ChefExpress chefExpress;
     private List<Recipe> unsortedRecipes;
     private List<Recipe> sortedRecipes;
