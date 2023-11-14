1142ebdd8acc19b5241686f04263440f066148e2
agusvidal <agustinacandela2300@gmail.com>
2023-11-14 17:27:49 -0300

    cambio en factory updater

--- src/main/java/factories/RecipesUpdaterFactory.java
+++ src/main/java/factories/RecipesUpdaterFactory.java
@@ -5,6 +5,8 @@ import entities.Recipe;
 import finders.LocalRecipesFactory;
 import interfaces.RecipesFactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
@@ -15,11 +17,23 @@ public class RecipesUpdaterFactory {
 
     }
 
-    public RecipesUpdater  createRecipesUpdater(Properties chefExpressProperties){
+    public RecipesUpdater  createRecipesUpdater(String propertyPath){
+        Properties chefExpressProperties = loadProperties(propertyPath);
         RecipesFactory recipesLocalFinder = new LocalRecipesFactory();
         Set<Recipe> recipes = recipesLocalFinder.findRecipes("/"+ chefExpressProperties.getProperty("RecipesPath"));
         String recipesPath = chefExpressProperties.getProperty("RecipesUpdaterPath");
         RecipesUpdater recipesUpdater = new RecipesUpdater(recipesLocalFinder,  List.of(recipesPath.split(",")),  recipes);
         return recipesUpdater;
+    }
+
+    private Properties loadProperties(String configPath)
+    {
+        Properties prop = new Properties();
+        try
+        {
+            prop.load( new FileInputStream(configPath) );
+        } catch (IOException e) { e.printStackTrace(); }
+
+        return prop;
     }
 }
